85986a1d7eadec06f93f2c8624297850f0622008
Sandro Pinheiro Lopes <sandro.lopes@ans.gov.br>
2020-11-09 20:46:44 -0300

    Correction of the ID type

--- src/main/java/com/github/product/api/repositories/ProductRepository.java
+++ src/main/java/com/github/product/api/repositories/ProductRepository.java
@@ -1,13 +1,11 @@
 package com.github.product.api.repositories;
 
-import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.github.product.api.entities.Product;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, UUID>{
+public interface ProductRepository extends JpaRepository<Product, Integer>{
 
 }
