0c7a1f2940168ebcb3f127f7e1e1ea9e1a68d6d1
Sandro Pinheiro Lopes <sandro.lopes@ans.gov.br>
2020-11-09 20:45:47 -0300

    Basic implementation of the methods

--- src/main/java/com/github/product/api/services/ProductService.java
+++ src/main/java/com/github/product/api/services/ProductService.java
@@ -1,35 +1,41 @@
 package com.github.product.api.services;
 
-import java.util.UUID;
+import java.math.BigDecimal;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.github.product.api.entities.Product;
+import com.github.product.api.repositories.ProductRepository;
 
 @Service
 public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
 
-	public void create(@Valid Product product) {
-		// TODO Auto-generated method stub
-		
+	public Product create(@Valid Product product) {
+		// TODO check if there is product with the same name.
+		return repository.save(product);
+	}
+	
+	public Product update(@Valid Product product) {
+		// TODO check if there is another product (different id) with the same name.
+		return repository.save(product);
 	}
 
-	public Product find(UUID id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Optional<Product> find(Integer id) {
+		return repository.findById(id);
 	}
 
-	public Page<Product> search() {
-		// TODO Auto-generated method stub
-		return null;
+	public Page<Product> search(String name, BigDecimal price, Integer quantity, Pageable pageable) {
+		Example<Product> example = Example.of(new Product(name, price, quantity));
+		return repository.findAll(example, pageable);
 	}
-
-	public void update(@Valid Product product) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
